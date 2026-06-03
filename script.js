8d2d4d02d9f0e851763f19d82c7f5dfe6689df99
Kaviyarasi Raju <kaviyarasiraju79@gmail.com>
2026-06-03 19:36:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+const form = document.getElementById("form");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const age = document.getElementById("age").value;
+  const name = document.getElementById("name").value;
+
+  if (age === "" || name.trim() === "") {
+    alert("Please enter valid details.");
+    return;
+  }
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Number(age) > 18) {
+        resolve(`Welcome, ${name}. You can vote.`);
+      } else {
+        reject(`Oh sorry ${name}. You aren't old enough.`);
+      }
+    }, 4000);
+  });
+
+  promise
+    .then((message) => alert(message))
+    .catch((message) => alert(message));
+});
